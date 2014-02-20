1102f3eed4db32f7f39e6ecbd3862984760590af
vilmacs <vilmacs@users.noreply.github.com>
2014-02-20 17:05:17 -0500

    mainly reformating

--- chart.js
+++ chart.js
@@ -2,205 +2,260 @@ var data
 var chart;
 var treeData;
 var treeChart;
+var filteredColumns = [];
+var series = {};
+
+
 var options = {
-    title: 'Charges',
-    titlePosition: 'in',
-    axisTitlesPosition: 'in',
-    hAxis: {textPosition: 'in'},
-    vAxis: {textPosition: 'in'},
-    curveType:'function',
-    focusTarget:'datum',
-    width: '1200',
-    height: 400,
-    legend: {position: 'top', maxLines:100,alignment:'center'},
-    chartArea:{height:'50%',width:'90%',left:0,top:170}
+  series: series,
+  title: 'Charges',
+  titlePosition: 'in',
+  axisTitlesPosition: 'in',
+  hAxis: {textPosition: 'in'},
+  vAxis: {textPosition: 'in'},
+  curveType:'function',
+  focusTarget:'datum',
+  width: '1200',
+  height: 400,
+  legend: {position: 'top', maxLines:100,alignment:'center'},
+  chartArea:{height:'50%',width:'90%',left:0,top:170}
 };
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
-    angular.bootstrap(document.body);
+  angular.bootstrap(document.body,['BillingExport']);
 }
+
+
+var app = angular.module('BillingExport', ['ngRoute']);
+app.config(
+  function($routeProvider){
+    $routeProvider.when('/',
+                        {templateUrl:'ShowAlerts',
+                         controller: 'ShowAlerts'});
+    $routeProvider.when('/AddAlert',
+                        {templateUrl:'AddAlert',
+                         controller: 'AddAlert'});
+  });
+
+
+app.controller('AddAlert', function ($scope,$http){
+  $scope.message = "Add An Alert!";
+});
+
+app.controller('ShowAlerts', function ($scope,$http){
+  $scope.message = "Here are your alerts!";
+});
+
+
+
+app.controller('BillingExportController', function ($scope,$http){
+  console.log("billingexportcontrollerinit");
+
+  $scope.showLoading=true;
+  function showLoading(show){
+    $scope.showLoading=show;
+  }
+
+  function showSelectedSku(selectedProduct,selectedSku){
+    // if selectedProduct and selectedSku are null.
+    // merge the
+
+    filteredColumns =[];
+    var filteredSeries = options.series;
+    if(typeof filteredSeries == 'undefined'){
+      filteredSeries = {};
+      options.series = filteredSeries;
+    }
+    for(var col=0;col < data.getNumberOfColumns();col++){
+      filteredColumns.push(col);
+      // skip the time column
+      if(col==0){
+        continue;
+      }
+      var columnId = data.getColumnId(col);
+      var product = columnId.substr(0,columnId.indexOf('/'));
+      var sku = columnId.substr(columnId.indexOf('/')+1,columnId.length);
+      if(((typeof selectedProduct == 'undefined') ||
+          (selectedProduct == null) ||
+          (product == selectedProduct)) &&
+         ((typeof selectedSku == 'undefined') ||
+          (selectedSku == null) ||
+          (sku == selectedSku))){
+        filteredColumns[col] = col;
+        filteredSeries[col-1] = {visibleInLegend:'True'};
+      }else{
+        filteredColumns[col] = {
+          label: data.getColumnLabel(col),
+          type: data.getColumnType(col),
+          calc: function () {
+            return null;
+          }
+        };
+        filteredSeries[col-1] = {visibleInLegend:'False'};
+      }
+    }
+    var view = new google.visualization.DataView(data);
+    view.setColumns(filteredColumns);
+    chart.draw(view, options);
+  }
+
+  $http.get('/getProfile').success(function(data){
+    console.log('profilescope');
+    $scope.profile = data;
+  });
+
+
+  $scope.projectSelected = function(){
+    console.log('projectselected!');
+    var query = new google.visualization.Query('/chart?project=' + $scope.project);
+    query.send(function(response){
+      console.log('callback!');
+      handleQueryResponse(response);
+    });
+  };
+
+  console.log("projectlist");
+  $scope.project = null;
+  $http.get('/projectList').success(function(data){
+    console.log("projectlistsuscess");
+    $scope.projects = data;
+    $scope.project = $scope.projects[5];
+    $scope.projectSelected();
+  });
+
+  // get dataSource response from app engine. draw chart.
+  function handleQueryResponse(response){
+    console.log('handleQueryResponse!');
+    showLoading(false);
+    $scope.$apply();
+    data = response.getDataTable();
+    if(typeof data == 'undefined' || data == null){
+      console.log('null?!');
+      return;
+    }
+    chart = new google.visualization.LineChart(document.getElementById('line_chart_div'));
+    treeChart = new google.visualization.TreeMap(document.getElementById('tree_chart_div'));
+
+    google.visualization.events.addListener(treeChart, 'rollup', function () {
+      showSelectedSku('Cloud');
+    });
+
+    google.visualization.events.addListener(treeChart, 'select', function () {
+      var sel = treeChart.getSelection();
+      // we selected something from the treemap, show on the line chart only the selected product or sku.
+      if (sel.length > 0) {
+        var selectedRow = sel[0].row;
+        var selectedProduct = treeData.getValue(selectedRow,1);
+        var selectedSku = treeData.getValue(selectedRow,0);
+        // top level product was selected.
+        if(selectedProduct == 'Cloud'){
+          selectedProduct = selectedSku;
+          selectedSku = null;
+        }
+        showSelectedSku(selectedProduct,selectedSku);
+      }
+    });
+
+
+    for (var i = 0; i < data.getNumberOfColumns(); i++) {
+      filteredColumns.push(i);
+      if (i > 0) {
+        series[i - 1] = {};
+      }
+    }
+
+    // draw a treemap of the first row of the datable.
+    function drawTreeChart(rowIndex){
+
+      var previousSelection = treeChart.getSelection();
+      // now construct a new DataTable
+      var treeDataTable ={cols:[],rows:[]};
+      treeDataTable.cols.push({label:'Product',type:'string'});
+      treeDataTable.cols.push({label:'Sku',type:'string'});
+      treeDataTable.cols.push({label:'Cost',type:'number'});
+      // come up with a color???
+      //treeDataTable.cols.push({label:'Difference',type:'number'});
+
+      // translate the existing data format into a nested tree structure.
+      // node, parent, value, color
+      // first get all the root nodes, with total values
+      // oh, wait!
+      // skip first row (time)
+      for(var col=1;col < data.getNumberOfColumns();col++){
+        var columnId = data.getColumnId(col);
+        var sku = columnId.substr(columnId.indexOf('/')+1,columnId.length);
+        var product = columnId.substr(0,columnId.indexOf('/'));
+        var cell = data.getValue(rowIndex,col);
+        treeDataTable.rows.push({c:[{v:sku},{v:product},{v:cell}]});
+      }
+
+      // add top level root product
+      treeDataTable.rows.push({c:[{v:'Cloud'},null,0]});
+      treeData = new google.visualization.DataTable(treeDataTable);
+      treeChart.draw(treeData);
+      if(previousSelection.length > 0){
+        treeChart.setSelection(previousSelection);
+      }
+    }
+
+    function drawLineChart(){
+
+      google.visualization.events.addListener(chart, 'select', function () {
+        var sel = chart.getSelection();
+        // if selection length is 0, we deselected an element
+        if (sel.length > 0) {
+          // if row is undefined, we clicked on the legend
+          var col = sel[0].column;
+          if (typeof sel[0].row === 'undefined' || sel[0].row == null) {
+            if (filteredColumns[col] == col) {
+              // hide the data series
+              filteredColumns[col] = {
+                label: data.getColumnLabel(col),
+                type: data.getColumnType(col),
+                calc: function () {
+                  return null;
+                }
+              };
+
+              // grey out the legend entry
+              series[col - 1].color = '#CCCCCC';
+            }
+            else {
+              // show the data series
+              filteredColumns[col] = col;
+              series[col - 1].color = null;
+            }
+            var view = new google.visualization.DataView(data);
+            view.setColumns(filteredColumns);
+            chart.draw(view, options);
+          }else{
+            // we selected a row, update the treemap
+            drawTreeChart(sel[0].row);
+            // also highlight column selected in the legend.
+          }
+        }
+      });
+      //             chart.draw(data, options);
+      showSelectedSku('Cloud');
+    }
+
+    // being too fast causes chart to improperly draw.
+    window.setTimeout(function(){
+      drawLineChart();
+      var lastRow = data.getNumberOfRows()-1;
+      chart.setSelection([{row:lastRow,column:0}]);
+      drawTreeChart(lastRow);
+    },2);
+  }
+
+});
+
 
 // Load the Visualization API and the piechart package.
 google.load('visualization', '1.0', {'packages':['corechart','treemap']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.setOnLoadCallback(drawChart);
-
-function BillingExportController($scope,$http){
-    $scope.showLoading=true;
-    function showLoading(show){
-        $scope.showLoading=show;
-    }
-
-    $scope.product = 'ALL';
-    $scope.products = null;
-    function resourceSelected(){
-        var filteredColumns =[];
-        var filteredSeries = options.series;
-        if(typeof filteredSeries == 'undefined'){
-            filteredSeries = {};
-            options.series = filteredSeries;
-        }
-        for(var col=0;col < data.getNumberOfColumns();col++){
-            filteredColumns.push(col);
-            // skip the time column
-            if(col==0){
-                continue;
-            }
-            var label = data.getColumnLabel(col);
-            var product = label.substr(0,label.indexOf('/'));
-            if(($scope.product == 'ALL') || (product == $scope.product)){
-                filteredColumns[col] = col;
-                filteredSeries[col-1] = {visibleInLegend:'True'};
-            }else{
-                filteredColumns[col] = {
-                    label: data.getColumnLabel(col),
-                    type: data.getColumnType(col),
-                    calc: function () {
-                        return null;
-                    }
-                };
-                filteredSeries[col-1] = {visibleInLegend:'False'};
-            }
-        }
-        var view = new google.visualization.DataView(data);
-        view.setColumns(filteredColumns);
-        chart.draw(view, options);
-    }
-
-    $http.get('/getprofile').success(function(data){
-        $scope.profile = data;
-        $scope.initialized=true;
-        showLoading(false);
-    });
-
-
-    // get dataSource response from app engine. draw chart.
-    function handleQueryResponse(response){
-        data = response.getDataTable();
-        chart = new google.visualization.LineChart(document.getElementById('line_chart_div'));
-        treeChart = new google.visualization.TreeMap(document.getElementById('tree_chart_div'));
-
-        google.visualization.events.addListener(treeChart, 'select', function () {
-            var sel = chart.getSelection();
-                if (sel.length > 0) {
-
-                }else{
-
-                }
-        });
-
-
-        // draw a treemap of the first row of the datable.
-        function drawTreeChart(rowIndex){
-            // now construct a new DataTable
-            var treeDataTable ={cols:[],rows:[]};
-            treeDataTable.cols.push({label:'Product',type:'string'});
-            treeDataTable.cols.push({label:'Sku',type:'string'});
-            treeDataTable.cols.push({label:'Cost',type:'number'});
-            //treeDataTable.cols.push({label:'Difference',type:'number'});
-
-            // translate the existing data format into a nested tree structure.
-            // node, parent, value, color
-            // first get all the root nodes, with total values
-            var tree = {};
-            // skip first row (time)
-            for(var col=1;col < data.getNumberOfColumns();col++){
-                var label = data.getColumnLabel(col);
-                var sku = label.substr(label.indexOf('/')+1,label.length);
-                var root = label.substr(0,label.indexOf('/'));
-                if(typeof tree[root] == 'undefined'){
-                    tree[root] = 0;
-                }
-                var cell = data.getValue(rowIndex,col);
-                tree[root] += cell;
-                treeDataTable.rows.push({c:[{v:sku},{v:root},{v:cell}]});
-            }
-
-
-
-            // add roots
-            treeDataTable.rows.push({c:[{v:'Cloud'},null,0]});
-            $scope.products = ['ALL'];
-            for (root  in tree){
-                treeDataTable.rows.push({c:[{v:root},{v:'Cloud'},{v:tree[root]}]});
-                $scope.products.push(root);
-            }
-            $scope.$apply();
-
-            treeData = new google.visualization.DataTable(treeDataTable);
-            treeChart.draw(treeData);
-
-
-
-        }
-
-        function drawLineChart(){
-            // taken from http://jsfiddle.net/rzbc7/
-            // allow showing/hiding of series data by clicking on the series.
-            var columns = [];
-            var series = {};
-
-            for (var i = 0; i < data.getNumberOfColumns(); i++) {
-                columns.push(i);
-                if (i > 0) {
-                    series[i - 1] = {};
-                }
-            }
-
-            options.series = series;
-
-            google.visualization.events.addListener(chart, 'select', function () {
-                var sel = chart.getSelection();
-                // if selection length is 0, we deselected an element
-                if (sel.length > 0) {
-                    // if row is undefined, we clicked on the legend
-                    var col = sel[0].column;
-                    if (typeof sel[0].row === 'undefined' || sel[0].row == null) {
-                        if (columns[col] == col) {
-                            // hide the data series
-                            columns[col] = {
-                                label: data.getColumnLabel(col),
-                                type: data.getColumnType(col),
-                                calc: function () {
-                                    return null;
-                                }
-                            };
-
-                            // grey out the legend entry
-                            series[col - 1].color = '#CCCCCC';
-                        }
-                        else {
-                            // show the data series
-                            columns[col] = col;
-                            series[col - 1].color = null;
-                        }
-                        var view = new google.visualization.DataView(data);
-                        view.setColumns(columns);
-                        chart.draw(view, options);
-                    }else{
-                        // we selected a row, update the treemap
-                        drawTreeChart(sel[0].row);
-                        // also highlight column selected in the legend.
-
-
-                    }
-                }
-            });
-
-            chart.draw(data, options);
-        }
-
-        drawLineChart();
-        var lastRow = data.getNumberOfRows()-1;
-        chart.setSelection([{row:lastRow,column:0}]);
-        drawTreeChart(lastRow);
-    }
-
-    // load datatable from server
-    var query = new google.visualization.Query('/chart');
-    query.send(handleQueryResponse);
-}
--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-"""
-returns billing information.
+"""View billing export information.
+
+An app engine application for parsing, displaing and triggering alerts from
+cloud platform billing export data.
+
 """
 
 from datetime import datetime
 import json
+import logging
+import re
 import webapp2
 import cloudstorage as gcs
-from cloudstorage.common import local_run
+from cloudstorage import common as gcs_common
 import gviz_api
+import httplib2
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-# do convolutions to speak to remote cloud storage even when on a local
-# devserver.
-if local_run():
-  import httplib2
+
+def UseLocalGCS():
+  """Use the local GCS stub."""
+  gcs_common.set_access_token(None)
+
+
+def UseRemoteGCS():
+  """Use remote GCS via a signed certificate."""
   from oauth2client.client import SignedJwtAssertionCredentials
-  from cloudstorage import common
   http_object = httplib2.Http(timeout=60)
   service_account = '773711848681-fm7jaj9nmbh9sedcmq8otutunvq5etog@developer.gserviceaccount.com'
   private_key_pem_file = 'f3b1d264fec3033936ccf54413ff2bc72ecab698-privatekey.pem'
@@ -27,7 +36,15 @@ if local_run():
                                               scope)
   certificate.refresh(http_object)
   # TODO(bmenasha): refresh every hour
-  common.set_access_token(certificate.access_token)
+  gcs_common.set_access_token(certificate.access_token)
+
+
+# do convolutions to speak to remote cloud storage even when on a local
+# devserver.
+if gcs_common.local_run():
+  UseLocalGCS()
+
+BUCKET = '/platform-demo-billing-export'
 
 
 # cache chart data
@@ -35,29 +52,68 @@ class ChartData(ndb.Model):
   data_table = ndb.PickleProperty()
 
 
+class Projects(ndb.Model):
+  projects = ndb.PickleProperty()
+
+
 def GetCanonicalLineItem(line_item):
   return line_item.replace('com.google.cloud/services/', '')
 
 
-# figure out how to cache this stuff.
-def GetAllBillingDataTable():
+def AddCloudProductSums(line_items, date_hash):
+  """Synthesize product specific columns."""
+  line_item_product = [li.split('/')[0] for li in line_items]
+  for _, row in date_hash.iteritems():
+    product_totals = {li.split('/')[0]: 0 for li in line_items}
+    for i, cost in enumerate(row):
+      if cost is not None:
+        product_totals[line_item_product[i]] += cost
+    for _ in range(len(row), len(line_items)):
+      row.append(None)
+    for _, total in sorted(product_totals.iteritems()):
+      row.append(float(total))
+  line_items += ['Cloud/' + product
+                 for product in sorted(set(line_item_product))]
+
+
+def GetBillingProjects():
+  """return a list of all projects we have billing export informaiton for."""
+  projects = Projects.get_by_id('Projects')
+  if projects is not None:
+    return projects.projects
+  project_list = []
+  current_project = None
+  project_re = re.compile('.*/(.*)-[0-9]{4}-[0-9]{2}-[0-9]{2}.json')
+  for billing_object in gcs.listbucket(BUCKET,
+                                       delimiter='/'):
+    project_name_match = re.match(project_re, billing_object.filename)
+    if not project_name_match:
+      continue
+    project_name = project_name_match.group(1)
+    if current_project != project_name:
+      project_list.append(project_name)
+      current_project = project_name
+  projects = Projects(id='Projects')
+  projects.projects = project_list
+  projects.put()
+  return project_list
+
+
+def GetAllBillingDataTable(project_name):
   """Return billing table data."""
   # first example datastore cache.
-  cached_data_table = ChartData.get_by_id('ChartData')
+  cached_data_table = ChartData.get_by_id(project_name)
   if cached_data_table is not None:
     return cached_data_table.data_table
   line_items = []
   date_hash = dict()
-  bucket = '/platform-demo-billing-export'
-  for billing_object in gcs.listbucket(bucket + '/google-platform-demo-',
+  for billing_object in gcs.listbucket(BUCKET + '/' + project_name,
                                        delimiter='/'):
     billing_file = gcs.open(billing_object.filename)
     biling_data = json.loads(billing_file.read())
     for item in biling_data:
-      # parse iso datetime without timzone (python can't handle it)
       end_time = datetime.strptime(item['endTime'][:-6], '%Y-%m-%dT%H:%M:%S')
       line_item = GetCanonicalLineItem(item['lineItemId'])
-
       if line_item not in line_items:
         line_items.append(line_item)
       row = date_hash.get(end_time, [])
@@ -68,22 +124,26 @@ def GetAllBillingDataTable():
       row[coli] = float(item['cost']['amount'])
     billing_file.close()
 
-  data_table = gviz_api.DataTable([('Time', 'datetime')] +
-                                  [(line_item, 'number') for
-                                   line_item in line_items])
+  AddCloudProductSums(line_items, date_hash)
+  data_table = gviz_api.DataTable([('Time', 'datetime', 'Time')] +
+                                  [(li, 'number', li.split('/')[1])
+                                   for li in line_items])
   data_table_data = [[date] + row for date, row in date_hash.iteritems()]
   data_table.LoadData(data_table_data)
-  cached_data_table = ChartData(id='ChartData')
+  cached_data_table = ChartData(id=project_name)
   cached_data_table.data_table = data_table
   cached_data_table.put()
   return data_table
 
 
-class MainPage(webapp2.RequestHandler):
+class GetChartData(webapp2.RequestHandler):
   def get(self):
     """Render chart."""
-    data_table = GetAllBillingDataTable()
-    json_response = data_table.ToJSonResponse(columns_order=None,
+    data_table = GetAllBillingDataTable(self.request.get('project'))
+    tqx = self.request.get('tqx')
+    req_id = int(tqx[tqx.find('reqId'):].split(':')[1])
+    json_response = data_table.ToJSonResponse(req_id=req_id,
+                                              columns_order=None,
                                               order_by='Time')
     self.response.write(json_response)
 
@@ -91,7 +151,10 @@ class MainPage(webapp2.RequestHandler):
 class FlushCache(webapp2.RequestHandler):
   def post(self):
     """Clear Datastore cache."""
-    ndb.Key('ChartData', 'ChartData').delete()
+    chart_data_keys = ChartData.query().fetch(keys_only=True)
+    ndb.delete_multi_async(chart_data_keys)
+    project_list_keys = Projects.query().fetch(keys_only=True)
+    ndb.delete_multi_async(project_list_keys)
     self.redirect('/index.html')
 
 
@@ -103,7 +166,16 @@ class GetProfileInformation(webapp2.RequestHandler):
                            'logoutUrl': users.create_logout_url('/')}
     self.response.out.write(json.dumps(profile_information))
 
-app = webapp2.WSGIApplication([('/chart', MainPage),
-                               ('/getprofile', GetProfileInformation),
-                               ('/flushCache', FlushCache)],
-                              debug=True)
+
+class GetProjectList(webapp2.RequestHandler):
+  def get(self):
+    """Returns logged in user information."""
+    project_list = GetBillingProjects()
+    self.response.out.write(json.dumps(project_list))
+
+app = webapp2.WSGIApplication(
+    [('/chart', GetChartData),
+     ('/projectList', GetProjectList),
+     ('/getProfile', GetProfileInformation),
+     ('/flushCache', FlushCache)],
+    debug=True)
